445247956c23d1e72c3f8ed95be06087f33fda94
StarGrim33 <prosky33@gmail.com>
2022-07-10 11:12:11 +0300

    1
    First try

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace JuniorHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameUser;
+            char symbol;
+            int nameUserLength;
+
+            Console.WriteLine("Как Вас зовут ? ");
+            nameUser = Console.ReadLine();
+            Console.WriteLine("Какой символ Вы выберете ? ");
+            symbol = Convert.ToChar(Console.ReadLine());
+            nameUserLength = nameUser.Length;
+
+            for (int i = 0; i < nameUserLength + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine("\n" + symbol + nameUser + symbol);
+            for (int i = 0; i < nameUserLength + 2; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+    }
+}
